2641e3db714fd53e1f97b489a02e8d445e2790d9
iqbal07 <iqbalhossen0711@gmail.com>
2026-03-14 15:41:06 +0600

    register page has been created successfully

--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -6,24 +6,47 @@ function Register() {
 
     const handleSubmitForm = e => {
         e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        console.log(name,email, password);
     }
 
 
 
   return (
     <div className="mx-auto text-center py-20">
-      <form className="flex justify-center items-center">
+      <form
+        onSubmit={handleSubmitForm}
+        className="flex justify-center items-center"
+      >
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
           <legend className="fieldset-legend text-2xl">Register</legend>
 
           <label className="label">Name</label>
-          <input type="text" className="input" placeholder="Enter your name" name="name" />
-                  
+          <input
+            type="text"
+            className="input"
+            placeholder="Enter your name"
+            name="name"
+          />
+
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Enter your email" name="email" />
+          <input
+            type="email"
+            className="input"
+            placeholder="Enter your email"
+            name="email"
+          />
 
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Enter your password" name="password" />
+          <input
+            type="password"
+            className="input"
+            placeholder="Enter your password"
+            name="password"
+          />
 
           <button className="btn btn-neutral mt-4">Register</button>
         </fieldset>
