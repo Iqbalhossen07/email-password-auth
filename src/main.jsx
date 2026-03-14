0bf31a756c9bc00fb8e59463adf67a7df7d4e132
iqbal07 <iqbalhossen0711@gmail.com>
2026-03-14 15:07:47 +0600

    email password auth project has been setup successfully

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './Root/Root.jsx';
+import Home from './Pages/Home/Home.jsx';
+import About from './Pages/About/About.jsx';
+import Register from './Auth/Register.jsx';
+import Login from './Auth/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      { index: true, Component: Home },
+      { path: "/about", Component: About },
+      { path: "/register", Component: Register },
+      { path: "/login", Component: Login },
+     
+    ],
+  },
+]);
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
